3224858cbd8af1720f96ef5e2d109cadba0dd49a
anelnel <duenasjanel@gmail.com>
2026-03-13 13:57:16 +0800

    generated angular components and services

---
+++ car-app/src/app/components/cars/cars.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cars',
+  imports: [],
+  templateUrl: './cars.html',
+  styleUrl: './cars.css',
+})
+export class Cars {
+
+}
