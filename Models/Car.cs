e731c97551783cf81ea3f4d6c3b51f736c76a01e
anelnel <duenasjanel@gmail.com>
2026-03-13 11:56:12 +0800

    add Car Model and folder structure

---
+++ Models/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace CarApi.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public DateTime RegistrationExpiry { get; set; }
+    }
+}
