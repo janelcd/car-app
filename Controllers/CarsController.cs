b7c132bc261537b952a9dec2494b169cf3fa8324
anelnel <duenasjanel@gmail.com>
2026-03-13 12:26:40 +0800

    add cars controller with optional make filter

---
+++ Controllers/CarsController.cs
@@ -0,0 +1,23 @@
+﻿using CarApi.Data;
+using CarApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CarsController: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Car>> GetCars([FromQuery] string? make)
+        {
+            var cars = CarDataStore.Cars.AsEnumerable();
+            if (!string.IsNullOrWhiteSpace(make))
+            {
+                cars = cars.Where(c => c.Make.Equals(make, StringComparison.OrdinalIgnoreCase));
+            }
+
+            return Ok(cars);
+        }
+    }
+}
