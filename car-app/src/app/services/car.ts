5fef4397d530cc6161132e77d07ad430a3b4df69
anelnel <duenasjanel@gmail.com>
2026-03-13 14:37:24 +0800

    added car service

--- car-app/src/app/services/car.ts
+++ car-app/src/app/services/car.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
+import { Observable } from 'rxjs';
+
+export interface Car {
+    id: number;
+    make: string;
+    model: string;
+    year: number;
+    registrationExpiry: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
-export class Car {
+
+export class CarService {
+  private apiUrl = '/api/cars';
+
+  constructor(private http: HttpClient){}
+
+  getCars(make?: string): Observable<Car[]> {
+    const params: any = {};
+    if (make && make.trim() !== '') {
+      params['make'] = make;
+    }
+    return this.http.get<Car[]>(this.apiUrl, {params});
+  }
   
 }
